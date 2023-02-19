56fdaacebe7b29a89df6f6f3e0472626da0ff159
PanaMour <60044131+PanaMour@users.noreply.github.com>
2023-02-19 16:46:11 +0200

    Added Sir Gallahad and Treasure Platform

---
+++ Assets/C# Scripts/KnightController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+public class KnightController : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rigidbody;
+    private bool idle;
+    public float dis = 5f;
+    public Transform playerTransform;
+    private Quaternion rotation;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+        rotation = transform.rotation;
+        idle = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        idle = this.gameObject.GetComponent<NPCInteraction>().isStopping;
+        if (idle)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 3.0f);
+            animator.Play("Idle");
+        }
+        else
+        {
+            transform.LookAt(new Vector3(playerTransform.position.x, this.gameObject.transform.position.y, playerTransform.position.z));
+            animator.Play("Disbelief");
+        }
+    }
+}
