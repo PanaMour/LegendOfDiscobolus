364c26695527a63082c64d0017b9bf2b50021a68
PanaMour <60044131+PanaMour@users.noreply.github.com>
2023-03-01 16:43:26 +0200

    Added Invisible Trap Arena Cave Barriers

--- Assets/C# Scripts/Discus.cs
+++ Assets/C# Scripts/Discus.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Discus : MonoBehaviour
 {
     [SerializeField] private GameObject wall;
+    [SerializeField] private GameObject gate;
+    [SerializeField] private GameObject blocker1;
+    [SerializeField] private GameObject blocker2;
     private void OnTriggerEnter(Collider other)
     {
         PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
@@ -13,6 +16,9 @@ public class Discus : MonoBehaviour
             playerInventory.DiscusCollected();
             gameObject.SetActive(false);
             wall.SetActive(false);
+            gate.SetActive(false);
+            blocker1.GetComponent<NPCInteraction>().dialogue3 = true;
+            blocker2.GetComponent<NPCInteraction>().dialogue2 = true;
         }
     }
 }
---
+++ Assets/C# Scripts/TrapArena.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapArena : MonoBehaviour
+{
+    [SerializeField] private GameObject wall;
+    [SerializeField] private GameObject health;
+    [SerializeField] private GameObject trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            wall.SetActive(true);
+            health.SetActive(true);
+            trigger.SetActive(false);
+        }
+    }
+}
